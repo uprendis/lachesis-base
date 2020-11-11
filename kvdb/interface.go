584727d4dc64536dd73a27eeb41e0b647ae53ef2
Egor Lysenko <uprendis@gmail.com>
2020-11-11 16:42:01 +0300

    refactor Iteratee interface

--- kvdb/interface.go
+++ kvdb/interface.go
@@ -54,16 +54,7 @@ type Batcher interface {
 type Iteratee interface {
 	// NewIterator creates a binary-alphabetical iterator over the entire keyspace
 	// contained within the key-value database.
-	NewIterator() Iterator
-
-	// NewIteratorWithStart creates a binary-alphabetical iterator over a subset of
-	// database content starting at a particular initial key (or after, if it does
-	// not exist).
-	NewIteratorWithStart(start []byte) Iterator
-
-	// NewIteratorWithPrefix creates a binary-alphabetical iterator over a subset
-	// of database content with a particular key prefix.
-	NewIteratorWithPrefix(prefix []byte) Iterator
+	NewIterator(prefix []byte, start []byte) Iterator
 }
 
 // Store contains all the methods required to allow handling different
